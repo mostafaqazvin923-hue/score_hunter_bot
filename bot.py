212fdd210b243efe1c2a7026b328dac37921244b
mostafaqazvin923-hue <mostafaqazvin923@gmail.com>
2026-08-10 17:51:18 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,18 @@ import os
 import requests
 
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-response = requests.get(url)
+data = {
+    "chat_id": CHAT_ID,
+    "text": "🟢 Score Hunter Bot فعال شد"
+}
 
-print(response.status_code)
-print(response.text)
+response = requests.post(url, data=data, timeout=10)
+
+print("Status:", response.status_code)
+print("Telegram:", response.text)
+
+response.raise_for_status()
